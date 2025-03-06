3fe7cc529501abe806b580982d647399031903e3
richardhapb <richard.hapb@icloud.com>
2025-03-06 16:43:19 -0300

    update ghostty and nvim-update

--- scripts/.local/bin/nvim-update.sh
+++ scripts/.local/bin/nvim-update.sh
@@ -6,6 +6,11 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
                 sudo apt install glibc-source -y
         fi
     folder_name=nvim-linux-x86_64
+
+    # Raspberry Pi
+    if [[ $(uname -m) == "aarch64" ]]; then
+        folder_name=nvim-linux-arm64
+    fi
     wget https://github.com/neovim/neovim/releases/download/nightly/$folder_name.tar.gz -O $HOME/$name
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     folder_name=nvim-macos-arm64
@@ -18,4 +23,3 @@ tar -xvf $HOME/$name -C $HOME
 mv $HOME/$folder_name $HOME/nvim
 rm $HOME/$name
 ln -sf $HOME/nvim/bin/nvim $HOME/.local/bin/nvim
-source $HOME/.zshrc
