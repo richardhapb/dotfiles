d08babc73642b4b1ebf5b5ae5c8c6bc889d16a83
richardhapb <richard.hapb@icloud.com>
2025-04-15 21:46:37 -0400

    feat: mirror screen configuration

--- screens_layout/.screenlayout/clone.sh
+++ screens_layout/.screenlayout/clone.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# Necessary for detect HMDI
-xrandr --ouput HDMI-A-1 --off
+# Reset any previous configuration
+xrandr --output HDMI-A-0 --off
 xrandr --auto
 
-xrandr --output eDP --auto --output HDMI-A-1 --primary --auto --same-as eDP --scale-from 3840x2160 --dpi 220 --output DisplayPort-0 --off --output DisplayPort-1 --off
+# Configure displays:
+# - eDP: Internal laptop display
+# - HDMI-A-1: External display, mirrored with same content
+# - Scale from 4K (3840x2160) and set DPI for HiDPI display
+xrandr --output eDP --scale-from 3840x2160 --output HDMI-A-0 --primary --mode 3840x2160 --pos 0x0 --output DisplayPort-0 --off --output DisplayPort-1 --off
 
--- screens_layout/.screenlayout/home.sh
+++ screens_layout/.screenlayout/home.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Necessary for detect HMDI
-xrandr --ouput HDMI-A-0 --off
+xrandr --output HDMI-A-0 --off
 xrandr --auto
 
 xrandr --output eDP --off --output HDMI-A-0 --primary --dpi 220 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
