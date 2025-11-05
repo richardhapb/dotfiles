bfc295db1060495d4e8067488c898953ebdc3fd7
richardhapb <richard.hapb@icloud.com>
2025-11-04 23:19:32 -0300

    feat: update sessionizer and screens

--- screens_layout/.screenlayout/both.sh
+++ screens_layout/.screenlayout/both.sh
@@ -3,7 +3,6 @@
 xrandr --output HDMI-1 --off
 xrandr --auto
 
-
-xrandr --output eDP-1 --left-of HDMI-1 --scale 1x0.9 --output HDMI-1 --primary  --mode 3840x2160 --scale 0.5x0.5 --dpi 220 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --left-of HDMI-1 --output HDMI-1 --primary --dpi 220 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
 
 ./wallpaper.sh
